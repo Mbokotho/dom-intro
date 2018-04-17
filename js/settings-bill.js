2372dc7d0d837842f8823e740816db6c24f1051b
mbokotho <mplwando@gmail.com>
2018-04-17 09:53:17 +0200

    updated text bill

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -25,20 +25,6 @@ var warningLevel= 0;
 var sms = 0.0;
 var call = 0.00;
 
-// create a variables that will keep track of all three totals.
-
-//add an event listener for when the 'Update settings' button is pressed
-
-//add an event listener for when the add button is pressed
-
-
-//in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the call / sms total
-// * add the appropriate value to the overall total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen.
-// * check the value thresholds and display the total value in the right
-//get a reference to the add buttongs");
 
 function Settinngs(){
 
@@ -64,16 +50,17 @@ updateSettingsBtn.addEventListener('click', Settinngs);
         var billItemType = checkedRadioBtn.value;
         // billItemType will be 'call' or 'sms'
 }
+        if (Total >= criticalLevel) {
 
-  if(billItemType === "call"){
-          call += callCost;
-      }
-      else if (billItemType === "sms"){
-          sms += smsCost;
-      }
+        }else{
+          if(billItemType === "call"){
+                  call += callCost;
+              }
+              else if (billItemType === "sms"){
+                  sms += smsCost;
+              }
 
-
-
+        }
 
 Total = call + sms;
 smsTotalSettings.innerHTML = sms.toFixed(2);
@@ -82,11 +69,11 @@ totalSettings.innerHTML = Total.toFixed(2);
 
 
 
-  if (Total > criticalLevel){
+  if (Total >= criticalLevel){
        // adding the danger class will make the text red
        totalSettings.classList.add("danger");
    }
-   else if (Total >  warningLevel){
+   else if (Total >=  warningLevel){
        totalSettings.classList.add("warning");
    }
 
@@ -97,7 +84,7 @@ if (Total < criticalLevel) {
   totalSettings.classList.remove("danger");
 }
 
-if (billTotal < warningLevel) {
+if (Total < warningLevel) {
   totalSettings.classList.remove("warning");
 }
 
